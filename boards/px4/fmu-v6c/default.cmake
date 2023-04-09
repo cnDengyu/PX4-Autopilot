f8fcb6726e7e55716e711ca64622dd0b415b0202
littlesunny <littlesunny2001@163.com>
2023-04-09 22:43:10 +0800

    Add support for fmu-v6c boards

---
+++ boards/px4/fmu-v6c/default.cmake
@@ -0,0 +1,125 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v6c
+	LABEL default
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	UAVCAN_INTERFACES 2
+	UAVCAN_TIMER_OVERRIDE 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS0
+		GPS2:/dev/ttyS6
+		TEL1:/dev/ttyS5
+		TEL2:/dev/ttyS3
+		TEL3:/dev/ttyS1
+
+	DRIVERS
+		adc/board_adc
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		dshot
+		gps
+		heater
+		imu/bosch/bmi055
+		imu/invensense/icm42688p
+		lights
+		magnetometer # all available magnetometer drivers
+		optical_flow # all available optical flow drivers
+		power_monitor/ina226
+		# power_monitor/ina228
+		# power_monitor/ina238
+		pwm_out
+		pwm_out_sim
+		px4io
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		airspeed_selector
+		battery_status
+		camera_feedback
+		commander
+		control_allocator
+		dataman
+		ekf2
+		esc_battery
+		events
+		flight_mode_manager
+		fw_att_control
+		# fw_autotune_attitude_control
+		fw_pos_control_l1
+		# gimbal
+		gyro_calibration
+		gyro_fft
+		land_detector
+		landing_target_estimator
+		load_mon
+		logger
+		# mag_bias_estimator
+		# manual_control
+		mavlink
+		mc_att_control
+		# mc_autotune_attitude_control
+		mc_hover_thrust_estimator
+		mc_pos_control
+		mc_rate_control
+		navigator
+		rc_update
+		rover_pos_control
+		sensors
+		sih
+		temperature_compensation
+		vtol_att_control
+
+
+	SYSTEMCMDS
+		bl_update
+		dmesg
+		hardfault_log
+		i2cdetect
+		led_control
+		mft
+		mixer
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		sd_bench
+		serial_test
+		system_time
+		top
+		topic_listener
+		tune_control
+		uorb
+		ver
+		work_queue
+
+	EXAMPLES
+		fake_gps
+		#fake_imu
+		#fake_magnetometer
+		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		#hello
+		#hwtest # Hardware test
+		#matlab_csv_serial
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		#rover_steering_control # Rover example app
+		#uuv_example_app
+		#work_item
+
+	)
